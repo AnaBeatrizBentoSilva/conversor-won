4a0042c046a82d831b869e5cc01d20fab95c3213
AnaBeatrizBentoSilva <anabento3807@gmail.com>
2025-04-01 13:18:45 -0300

    Projeto conversor de wons

---
+++ script.js
@@ -0,0 +1,5 @@
+function conversorWons(){
+    let valorWons = prompt("Digite um valor em Wons coreanos (KRW) ");
+    const won = 0.0040; 
+    alert("O valor em reais é: R$ " + (valorWons * won));
+}
